6095dfa3a9aa9e684ce5aba24e70bbc2cb56b2af
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-11-10 15:20:36 -0600

    adjusted new candy form to update state on input change, and moved add candy to on form submit

--- candy-market.ui/src/components/candy-list.js
+++ candy-market.ui/src/components/candy-list.js
@@ -7,6 +7,11 @@ import candyData from '../data-request/candy-data'
 class CandyList extends React.Component {
   state = {
     candyList: [],
+    candy: {
+      name: '',
+      category: '',
+      manufacturer: '',
+    },
   }
 
   buildCandy = () => {
@@ -15,13 +20,25 @@ class CandyList extends React.Component {
     ));
   }
 
-  addCandy = () => {
-    const newCandy = {
-      name: document.getElementById('candy-name').value,
-      category: document.getElementById('candy-category').value,
-      manufacturer: document.getElementById('candy-manufacturer').value,
-    }
+  updateCandyForm = (field, event) => {
+    const candy = { ...this.state.candy };
+    candy[field] = event.target.value;
+    this.setState({ candy })
+  }
 
+  updateName = event => this.updateCandyForm('name', event);
+  updateCategory = event => this.updateCandyForm('category', event);
+  updateManufacturer = event => this.updateCandyForm('manufacturer', event);
+
+  addCandy = (event) => {
+    event.preventDefault();
+    const newCandy = { ...this.state.candy };
+    const candyDefault = {
+      name: '',
+      category: '',
+      manufacturer: '',
+    }
+    this.setState({ candy: candyDefault });
     candyData.addCandy(newCandy)
     .then(() => this.getCandy()).catch();
   }
@@ -42,37 +59,39 @@ class CandyList extends React.Component {
   }
 
   render() {
+    const { candy } = this.state;
     return (
       <div className="container mt-5">
         <div className="lead">
           <span className="mr-3">Candy Market Bois!</span>
 
-          <span className="blerg">
-  <i class="fas fa-plus pointer" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-  </i>
-</span>
-<div class="collapse" id="collapseExample">
-  <div class="card card-body">
-  <div class="form-group">
-    <label for="candy-name">Name</label>
-    <input type="text" class="form-control" id="candy-name" aria-describedby="emailHelp" placeholder="Enter Name"/>
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="candy-category">Category</label>
-    <input type="text" class="form-control" id="candy-category" placeholder="Category"/>
-  </div>
-  <div class="form-group">
-    <label for="candy-manufacturer">Manufacturer</label>
-    <input type="text" class="form-control" id="candy-manufacturer" placeholder="Manufacturer"/>
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-    <label class="form-check-label" for="exampleCheck1">Checkbox does nothing!</label>
-  </div>
-  <button class="btn btn-primary" onClick={this.addCandy}>Submit</button>
-  </div>
-</div>
+          <span>
+            <i className="fas fa-plus pointer" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"></i>
+          </span>
+          <form onSubmit={this.addCandy}>
+            <div className="collapse" id="collapseExample">
+              <div className="card card-body">
+              <div className="form-group">
+                <label htmlFor="candy-name">Name</label>
+                <input type="text" className="form-control" id="candy-name" aria-describedby="emailHelp" placeholder="Enter Name" value={candy.name} onChange={this.updateName}/>
+                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+              </div>
+              <div className="form-group">
+                <label htmlFor="candy-category">Category</label>
+                <input type="text" className="form-control" id="candy-category" placeholder="Category" value={candy.category} onChange={this.updateCategory}/>
+              </div>
+              <div className="form-group">
+                <label htmlFor="candy-manufacturer">Manufacturer</label>
+                <input type="text" className="form-control" id="candy-manufacturer" placeholder="Manufacturer" value={candy.manufacturer} onChange={this.updateManufacturer}/>
+              </div>
+              <div className="form-group form-check">
+                <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                <label className="form-check-label" htmlFor="exampleCheck1">Checkbox does nothing!</label>
+              </div>
+              <button type="submit" className="btn btn-primary">Submit</button>
+              </div>
+            </div>
+          </form>
         </div>
 
         <div className="row mt-5">
