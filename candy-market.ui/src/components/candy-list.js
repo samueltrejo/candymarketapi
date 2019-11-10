c9e68a0df302d688323767d0d10df6fa9b482684
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-11-10 13:15:04 -0600

    adding and deleting candy

--- candy-market.ui/src/components/candy-list.js
+++ candy-market.ui/src/components/candy-list.js
@@ -11,20 +11,73 @@ class CandyList extends React.Component {
 
   buildCandy = () => {
     return this.state.candyList.map((candy) => (
-      <Candy key={candy.id} candy={candy} />
+      <Candy key={candy.id} candy={candy} deleteCandy={this.deleteCandy} />
     ));
   }
 
-  componentDidMount() {
+  addCandy = () => {
+    const newCandy = {
+      name: document.getElementById('candy-name').value,
+      category: document.getElementById('candy-category').value,
+      manufacturer: document.getElementById('candy-manufacturer').value,
+    }
+
+    candyData.addCandy(newCandy)
+    .then(() => this.getCandy()).catch();
+  }
+
+  deleteCandy = (id) => {
+    candyData.deleteCandy(id)
+      .then(() => this.getCandy()).catch();
+  }
+
+  getCandy = () => {
     candyData.getCandy()
       .then(candyList => this.setState({ candyList }))
       .catch(error => console.error(error));
   }
 
+  componentDidMount() {
+    this.getCandy();
+  }
+
   render() {
     return (
-      <div>
-        {this.buildCandy()}
+      <div className="container mt-5">
+        <div className="lead">
+          <span className="mr-3">Candy Market Bois!</span>
+
+          <span className="blerg">
+  <i class="fas fa-plus pointer" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+  </i>
+</span>
+<div class="collapse" id="collapseExample">
+  <div class="card card-body">
+  <div class="form-group">
+    <label for="candy-name">Name</label>
+    <input type="text" class="form-control" id="candy-name" aria-describedby="emailHelp" placeholder="Enter Name"/>
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="candy-category">Category</label>
+    <input type="text" class="form-control" id="candy-category" placeholder="Category"/>
+  </div>
+  <div class="form-group">
+    <label for="candy-manufacturer">Manufacturer</label>
+    <input type="text" class="form-control" id="candy-manufacturer" placeholder="Manufacturer"/>
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+    <label class="form-check-label" for="exampleCheck1">Checkbox does nothing!</label>
+  </div>
+  <button class="btn btn-primary" onClick={this.addCandy}>Submit</button>
+  </div>
+</div>
+        </div>
+
+        <div className="row mt-5">
+          {this.buildCandy()}
+        </div>
       </div>
     );
   }
--- candy-market.ui/src/components/candy.js
+++ candy-market.ui/src/components/candy.js
@@ -1,14 +1,29 @@
 import React from 'react';
 
 class Candy extends React.Component {
+  deleteCandy = () => {
+    this.props.deleteCandy(this.props.candy.id);
+  }
+
   render() {
     const { candy } = this.props;
     return (
-      <div className="Candy">
-        <div>{candy.name}</div>
+      <div className="Candy col-md-6 col-lg-4 mb-4">
+        <div className="card">
+          <div className="card-body">
+            <div className="d-flex justify-content-between">
+              <div></div>
+              <i className="fas fa-times pointer" onClick={this.deleteCandy}></i>
+            </div>
+            <div>{candy.name}</div>
+            <div>{candy.manufacturer}</div>
+            <div>{candy.category}</div>
+            <div>{candy.date}</div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default Candy;
+export default Candy; 
